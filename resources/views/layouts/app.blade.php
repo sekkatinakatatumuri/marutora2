0d5fff76c25e89efd0b1a040db3a5e930e3193ba
Tatsuya Fujii <sekkatinakatatumuri@gmail.com>
2017-11-24 04:11:07 +0000

    delete ogp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,6 @@
 <html>
 <head>
 
-<!--OGP-->
-<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
-<meta property="og:title" content="Marutora" />
-<meta property="og:type" content="travel" />
-<meta property="og:url" content="http://marutora.herokuapp.com/" />
-<meta property="og:image" content="http://marutora.herokuapp.com/images/logo.png" />
-<meta property="og:site_name" content="Marutora" />
-<meta property="og:description" content="旅先の情報をまるっと表示する" />
-
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -23,11 +14,8 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 <!--bootstrap-select-->
-<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
-<!-- Latest compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
-<!-- (Optional) Latest compiled and minified JavaScript translation files -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/i18n/defaults-*.min.js"></script>
 
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
